f91a197f0e3956643d2477680086a7b007b5609c
JuanCSoria <usuariojcs@gmail.com>
2023-04-15 06:43:04 -0300

    mejora en aspecto y animaciones, ajustes responsivos, barra de scroll y otros

---
+++ src/app/componentes/noencontrada/noencontrada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noencontrada',
+  templateUrl: './noencontrada.component.html',
+  styleUrls: ['./noencontrada.component.css']
+})
+export class NoencontradaComponent {
+
+}
